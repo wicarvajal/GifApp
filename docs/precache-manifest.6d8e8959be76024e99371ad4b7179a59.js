6991405405f088fb54ef8f94fae854816139eeb4
wicarvajal <wi.rcarvajal@gmail.com>
2020-07-19 01:26:11 -0400

    add dist files

---
+++ docs/precache-manifest.6d8e8959be76024e99371ad4b7179a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3783e4d1815e2443f27ef2e131f540e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db12b60644c03f2be4a1",
+    "url": "/static/css/main.fc9f75c4.chunk.css"
+  },
+  {
+    "revision": "28ec7e84b06b3fa02055",
+    "url": "/static/js/2.b8815147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8815147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db12b60644c03f2be4a1",
+    "url": "/static/js/main.15ed4129.chunk.js"
+  },
+  {
+    "revision": "b79b82474f8643c90541",
+    "url": "/static/js/runtime-main.0a577870.js"
+  }
+]);
